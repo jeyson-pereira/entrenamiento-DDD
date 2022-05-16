133b928e399b193e6b8402c27fa861944f5f624d
Jeyson Alejandro Pereira <jpereiraf96@gmail.com>
2022-05-16 18:08:51 -0500

    Validaciones de Directriz y generar id mentoria por parametro

--- domain/src/main/java/co/com/sofkau/entrenamiento/curso/values/Directiz.java
+++ domain/src/main/java/co/com/sofkau/entrenamiento/curso/values/Directiz.java
@@ -2,9 +2,25 @@ package co.com.sofkau.entrenamiento.curso.values;
 
 import co.com.sofka.domain.generic.ValueObject;
 
+import java.util.Objects;
+
 public class Directiz implements ValueObject<String> {
+    private final String value;
+
+    public Directiz(String value) {
+        this.value = Objects.requireNonNull(value);
+
+        if(this.value.isBlank()){
+            throw new IllegalArgumentException("El nombre no puede estar en blanco");
+        }
+
+        if(this.value.length() > 150){
+            throw new IllegalArgumentException("La directriz no permite mas de 150 caracteres");
+        }
+    }
+
     @Override
     public String value() {
-        return null;
+        return value;
     }
 }
